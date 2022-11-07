d71bfad4ee24136119a6788dfb5921f39458afdd
ivanleonov <treska1315@gmail.com>
2022-11-07 13:13:48 +0300

    added texts i18next

---
+++ src/locales/en.js
@@ -0,0 +1,37 @@
+export default {
+  translation: {
+    signupPage: {
+      header: 'Sign up',
+      labels: {
+        name: 'Name',
+        password: 'Password',
+        confirmPassword: 'Confirm password',
+      },
+      submitButton: 'Sign up',
+    },
+    loginPage: {
+      header: 'Log in',
+      labels: {
+        name: 'Name',
+        password: 'Password',
+      },
+      submitButton: 'Log in',
+      footer: {
+        span: 'Don`t have account yet?',
+        link: 'Registration',
+      },
+    },
+    feedback: {
+      required: 'Field is required',
+      toShortPassword: 'Password is too short',
+      notMatches: 'Should match password',
+      toLong: 'Not more than 20 symbols',
+    },
+    mainPage: {
+    },
+    nav: {
+      AppName: 'Movie agrigator',
+      logoutButton: 'Log out',
+    },
+  },
+};
