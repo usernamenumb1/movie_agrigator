9cc090f247d65b7f9c5998188dcc461710db5c51
ivanleonov <treska1315@gmail.com>
2022-11-07 13:17:42 +0300

    added styles & plugged all above features

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './components/context/AuthProvider';
+import resources from './locales/index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const i18n = i18next.createInstance();
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  debug: true,
+  resources,
+}).then(() => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+});
