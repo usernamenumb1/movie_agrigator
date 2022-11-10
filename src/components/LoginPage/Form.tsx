a436b91280b43430c2865d526643c70ccfa8e3bd
ivanleonov <treska1315@gmail.com>
2022-11-10 18:26:51 +0300

    removed guts to Form component from LogIn

--- src/components/LoginPage/Form.tsx
+++ src/components/LoginPage/Form.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import routes from '../../routes';
-import { formProps } from '../../interfaces';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { AuthorizationContext } from "../context/AuthProvider";
+import routes from "../../routes";
+import { InitialFormValues, LogInArgs } from "../../interfaces";
 
-export default function Form(props: formProps) {
-  const {
-    username,
-    password,
-    logIn,
-    changeValue,
-    usernameError,
-    passwordError,
-  } = props;
+export default function Form() {
+  const { logIn } = useContext(AuthorizationContext);
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const logInUser = ({ data }: { data: LogInArgs }) => {
+    logIn(data);
+    navigate(routes.mainChatPage());
+  };
+
+  const formik = useFormik<InitialFormValues>({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .max(20, t("feedback.toLong") as string)
+        .required(t("feedback.required") as string),
+      password: Yup.string()
+        .min(5, t("feedback.toShortPassword") as string)
+        .required(t("feedback.required") as string),
+    }),
+    onSubmit: ({ username, password }) => {
+      axios
+        .post(routes.loginPath(), { username, password })
+        .then(logInUser)
+        .catch(({ response }) => {
+          if (response.status === 401) {
+            formik.setErrors({ username: t("feedback.unauthorized") });
+          }
+        });
+    },
+  });
+
+  const { username: usernameError, password: passwordError } = formik.errors;
+  const { username, password } = formik.values;
+
   return (
     <div className="card shadow-sm">
       <div className="card-body row p-5">
-        <form onSubmit={(e) => { e.stopPropagation(); e.preventDefault(); logIn(e); }} className="col-12 mt-3 mt-md-0">
-          <h1 className="text-center mb-4">{t('loginPage.header')}</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            formik.handleSubmit(e);
+          }}
+          className="col-12 mt-3 mt-md-0"
+        >
+          <h1 className="text-center mb-4">{t("loginPage.header")}</h1>
           <div className="form-floating mb-4">
             <input
               id="username"
@@ -26,11 +63,17 @@ export default function Form(props: formProps) {
               type="text"
               className="form-control"
               placeholder="Name"
-              onChange={changeValue}
+              onChange={formik.handleChange}
               value={username}
             />
-            <label htmlFor="username" className="form-label">{t('loginPage.labels.name')}</label>
-            {usernameError && <div className="custom-tooltip"><div className="tooltip-text">{usernameError}</div></div>}
+            <label htmlFor="username" className="form-label">
+              {t("loginPage.labels.name")}
+            </label>
+            {formik.errors.username && (
+              <div className="custom-tooltip">
+                <div className="tooltip-text">{usernameError}</div>
+              </div>
+            )}
           </div>
           <div className="form-floating mb-4">
             <input
@@ -39,19 +82,27 @@ export default function Form(props: formProps) {
               type="text"
               className="form-control"
               placeholder="Password"
-              onChange={changeValue}
+              onChange={formik.handleChange}
               value={password}
             />
-            <label htmlFor="password" className="form-label">{t('loginPage.labels.password')}</label>
-            {passwordError && <div className="custom-tooltip"><div className="tooltip-text">{passwordError}</div></div>}
+            <label htmlFor="password" className="form-label">
+              {t("loginPage.labels.password")}
+            </label>
+            {passwordError && (
+              <div className="custom-tooltip">
+                <div className="tooltip-text">{passwordError}</div>
+              </div>
+            )}
           </div>
-          <button type="submit" className="w-100 mb-3 btn btn-lightblue">{t('loginPage.submitButton')}</button>
+          <button type="submit" className="w-100 mb-3 btn btn-lightblue">
+            {t("loginPage.submitButton")}
+          </button>
         </form>
       </div>
       <div className="card-footer p-4 bckgrnd-lightblue">
         <div className="text-center">
-          <span className="pe-1">{t('loginPage.footer.span')}</span>
-          <Link to={routes.signUpPage()}>{t('loginPage.footer.link')}</Link>
+          <span className="pe-1">{t("loginPage.footer.span")}</span>
+          <Link to={routes.signUpPage()}>{t("loginPage.footer.link")}</Link>
         </div>
       </div>
     </div>
