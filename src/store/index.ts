1a1b7a167e53a10c1a25016c052910699f11b173
ivanleonov <treska1315@gmail.com>
2022-11-27 20:12:34 +0300

    delete loger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,17 @@
-import { configureStore, combineReducers, Middleware } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import movieSlice from "./slices/MovieSlice";
 import moviesAPI from "./API/MoviesAPI";
 import userDataApi from "./API/UserDataAPI";
 
-const logger: Middleware = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.log('current state', store.getState());
-  console.info('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd();
-  return result;
-};
+// const logger: Middleware = (store) => (next) => (action) => {
+//   console.group(action.type);
+//   console.log('current state', store.getState());
+//   console.info('dispatching', action);
+//   const result = next(action);
+//   console.log('next state', store.getState());
+//   console.groupEnd();
+//   return result;
+// };
 
 const rootReducer = combineReducers({
   movieSlice,
@@ -22,5 +22,5 @@ const rootReducer = combineReducers({
 export default () => configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .concat([moviesAPI.middleware, userDataApi.middleware, logger]),
+    .concat([moviesAPI.middleware, userDataApi.middleware]),
 });
